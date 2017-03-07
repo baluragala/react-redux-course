feb01f4b0fe5607771bb9191cadc33fb2a87d139
Bala Krishna Ragala <baluragala@sparity.com>
2017-03-07 23:09:08 +0530

    webpack HMR

---
+++ 0-webpack-react-DS/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="header">Hello, world.</h1>
+        <p>This is paragraph</p>
+      </div>
+    );
+  }
+}
